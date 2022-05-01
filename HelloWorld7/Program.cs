0535abe8bc2fe3ecfb0e368e5414ce902ca7f74e
ROXANA lazarescu <roxana_lazarescu13@yahoo.com>
2022-05-01 16:31:43 +0300

    Add project files.

---
+++ HelloWorld7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a");
+            int a = 20;
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("par");
+            }
+
+            else
+            {
+                Console.WriteLine("IMPAR)");
+            }
+
+        }
+    }
+}
